45845b433747d25c3ed270f03fc740a078719b58
Michael Kuehl <hello@txs.vc>
2022-06-28 10:08:46 +0200

    more config options in the Dockerfile and start.sh

--- base-notebook/start.sh
+++ base-notebook/start.sh
@@ -6,4 +6,11 @@ if [[ "${NOTEBOOK_ARGS} $*" != *"--ip="* ]]; then
     NOTEBOOK_ARGS="--ip=0.0.0.0 ${NOTEBOOK_ARGS}"
 fi
 
+# set default to $DATA_ROOT
+if [[ "${NOTEBOOK_ARGS} $*" != *"--notebook-dir="* ]]; then
+    NOTEBOOK_ARGS="--notebook-dir=${DATA_ROOT} ${NOTEBOOK_ARGS}"
+fi
+
+echo " --> jupyter ${JUPYTER_CMD} ${NOTEBOOK_ARGS}"
+
 exec jupyter ${JUPYTER_CMD} ${NOTEBOOK_ARGS} "$@"
